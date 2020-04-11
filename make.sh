3d7f17afd08dca45a82a90df3fc77f7f373b866a
chenjun <1378485490@qq.com>
2020-04-11 12:51:58 +0800

    修改脚本

--- make.sh
+++ make.sh
@@ -6,5 +6,6 @@ git checkout feature/cdf-ok-kline
 yarn install
 yarn run build 
 mv dist ../dist/kline
+rm -rf hqchart_kline/
 docker build -t registry.cn-hongkong.aliyuncs.com/newcoin/cfd-h5:liteex-pro .
 docker push registry.cn-hongkong.aliyuncs.com/newcoin/cfd-h5:liteex-pro
